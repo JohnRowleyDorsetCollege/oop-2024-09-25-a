edac23c0cf27512278388b7fca93bc729df2f238
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2024-09-25 09:28:30 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using oop_2024_09_25_a.models;
+
+Console.WriteLine("Inheritance");
+
+var hb = new HeroBase();
+var ap = new AlienPlant();
+var ah = new HumanAugmented();
+
+Console.WriteLine(hb);
+Console.WriteLine(ap);
+Console.WriteLine(ah);
---
+++ models/HeroBase.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_2024_09_25_a.models
+{
+    public class HeroBase
+    {
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+    public class Human : HeroBase
+    {
+        public override string ToString()
+        {
+            return "Human";
+        }
+    }
+
+    public class Alien : HeroBase
+    {
+
+        public override string ToString()
+        {
+            return "Alien";
+        }
+    }
+
+    public class HumanAugmented : Human
+    {
+        public override string ToString()
+        {
+            return "HumanAugmented";
+        }
+    }
+
+    public class AlienPlant : Alien
+    {
+        public override string ToString()
+        {
+            return "AlienPlant";
+        }
+    }
+
+}
